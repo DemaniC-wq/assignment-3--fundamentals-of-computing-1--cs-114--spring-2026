47001530d28b6388dd265524efd11504b1154dab
DemaniC-wq <demclarke@hartford.edu>
2026-04-14 13:46:15 -0400

    Added private class for matrix

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,4 +1,37 @@
 public class Diamond {
   public static void main(String[] args) {
+<<<<<<< Updated upstream
+=======
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a number for the size of the diamond: ");
+
+    int diamondSize = input.nextInt();
+    int oddDiamond;
+
+    if (diamondSize % 2 == 0) {
+    evenDiamond(diamondSize);
+} else {
+    oddDiamond(diamondSize);
+
+>>>>>>> Stashed changes
   }
-}
+      }
+      //Method for the odd Diamond
+      public static void oddDiamond(int size) {
+        int middleRow = size / 2;
+        for(int row = 0 ; row < size ; row++) {
+          int stars;
+
+          if(row<= middleRow){
+            stars= 2 * row + 1;
+          } else {
+            stars = 2 * (size - row - 1) + 1;
+          }
+
+        }
+  }
+  // This is for the even Diamond
+  public static void evenDiamond(int size) {
+    int halfDiamond = size / 2;
+
+}}
--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -2,5 +2,10 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    private int [][] matrix;
+    private int size
+
+    public(int size) {
+      this.size = size;
   }
 }
