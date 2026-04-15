3e39b0fdfc81e081e36da1be245c5116aa772b50
DemaniC-wq <demclarke@hartford.edu>
2026-04-15 15:23:14 -0400

    Added a swap method

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -40,6 +40,10 @@ public class Matrix {
         System.out.println();
 
       };
+    private void swap(int row1, int col1, int row2, int col2) {
+      int temp = matrix[row1][col1];
+      matrix[row1][col1] = matrix[row2][col2];
+      matrix[row2][col2] = temp;
 
     }
   }
