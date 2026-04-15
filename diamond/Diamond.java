6397ebb745a8759bf7b29155491d08e7b26c9a24
DemaniC-wq <demclarke@hartford.edu>
2026-04-15 16:42:38 -0400

    Completed Diamond Program

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -3,6 +3,7 @@ public class Diamond {
   public static void main(String[] args) {
 <<<<<<< Updated upstream
 =======
+
     Scanner input = new Scanner(System.in);
     System.out.print("Enter a number for the size of the diamond: ");
 
@@ -15,6 +16,7 @@ public class Diamond {
     oddDiamond(diamondSize);
 
 >>>>>>> Stashed changes
+    input.close();
   }
       }
       //Method for the odd Diamond
@@ -28,11 +30,49 @@ public class Diamond {
           } else {
             stars = 2 * (size - row - 1) + 1;
           }
+          int spaces = (size - stars) / 2;
+          for(int spaceCount=0; spaceCount < spaces; spaceCount++) {
+            System.out.print(" ");
+          }
+          for(int starCount=0; starCount < stars; starCount++) {
+            System.out.print("*");
+          }
+          System.out.println();
+        }
 
         }
   }
+
   // This is for the even Diamond
   public static void evenDiamond(int size) {
+
     int halfDiamond = size / 2;
 
+    // Top half
+    for (int row = 1; row <= halfDiamond; row++) {
+
+        for (int space = 0; space < halfDiamond - row; space++) {
+            System.out.print(" ");
+        }
+
+        for (int star = 0; star < 2 * row; star++) {
+            System.out.print("*");
+        }
+
+        System.out.println();
+    }
+
+    // Bottom half
+    for (int row = halfDiamond - 1; row >= 1; row--) {
+
+        for (int space = 0; space < halfDiamond - row; space++) {
+            System.out.print(" ");
+        }
+
+        for (int star = 0; star < 2 * row; star++) {
+            System.out.print("*");
+        }
+
+        System.out.println();
+    }
 }}
